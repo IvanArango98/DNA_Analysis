32716908018faaca3cacfd774016754c0fa871b6
IvanArango98 <ivanandres613@gmail.com>
2021-10-10 18:29:11 -0600

    Subirimagenes locales y webCam

--- dna-analysis/src/components/Mensajes/MensajeInicio.js
+++ dna-analysis/src/components/Mensajes/MensajeInicio.js
@@ -1,7 +1,10 @@
 export const Mensajes_Inicio = {
-    Titulo : "Somos Familiares",
+    Titulo : "¿Somos Familiares?",
     Instrucciones : "Seleccione algúna de las opciones:",
     Imagen1: "Seleccione Imagen 1",
     Imagen2: "Seleccione Imagen 2",
-    MensajeImagen: "Cargar Imágenes locales"
+    MensajeImagen: "Cargar Imágenes locales",
+    MensajeImagenWeb: "Tomese dos fotografías",
+    TomaFoto1: "Tomar Foto 1",
+    TomaFoto2: "Tomar Foto 2"
 }
